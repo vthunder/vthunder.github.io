eed233b8cc94546d8d0047fcae05cf1cb2989aef
Dan Mills <code@sandmill.org>
2013-05-14 16:34:48 -0700

    add facebook connect support through singly

--- js/app.js
+++ js/app.js
@@ -2,42 +2,102 @@ $(document).foundation();
 
 angular.module("app", [])
 
-  .factory('dbRoot', function() {
-    return new Firebase('https://openhome.firebaseio.com/');
-  })
+  .value('appurl', "http://localhost:8000/")
+  .value('firebaseurl', "https://openhome.firebaseio.com/")
+  .value('client_id', "798c72ca5049c8da83542ec260ecf9e9")
 
-  .factory('auth',
-           ['dbRoot', '$rootScope',
-            function(dbRoot, $rootScope) {
+  .config(['$locationProvider',
+        function($locationProvider) {
+//    $locationProvider
+//      .html5Mode(false)
+//      .hashPrefix('!');
+  }])
+
+  .factory('dbRoot', ['firebaseurl', function(firebaseurl) {
+    return new Firebase(firebaseurl);
+  }])
+
+  .factory('userSvc',
+           ['dbRoot', '$rootScope', '$http', '$window',
+            function(dbRoot, $rootScope, $http, $window) {
+
+    $rootScope.user = {
+      auth: {
+        lastError: null,
+      },
+      data: {}
+    };
+
     return {
+      login: function() {
+        this.client.login('persona');
+      },
+      logout: function() {
+        this.client.logout();
+        $rootScope.user.auth.firebase = null;
+      },
+      connect: function(service) {
+        var redirect_uri = encodeURIComponent(appurl);
+        var singly = $rootScope.user.auth.singly;
+        var access_token = singly? singly.access_token : "";
+
+        var url = "https://api.singly.com/oauth/authenticate" +
+          "?client_id=" + client_id +
+          "&redirect_uri=" + redirect_uri +
+          "&service=" + service +
+          access_token? ("&access_token=" + access_token) : "" +
+          "&response_type=token";
+
+        $window.location.href = url;
+      },
+      singlyConnected: function(access_token, account) {
+        $rootScope.user.auth.singly = {
+          access_token: access_token,
+          account: account
+        }
+        $http.get("https://api.singly.com/profile?access_token=" + access_token)
+          .success(function(data) {
+            $rootScope.user.data.profile = data;
+          })
+          .error(function(data) {
+            alert("error: " + data);
+          });
+      },
       client: new FirebaseAuthClient(dbRoot, function(error, user) {
         $rootScope.$apply(function() {
-          if (error) {
-            $rootScope.userLoginError = error;
-            $rootScope.user = null;
-          } else if (user) {
-            $rootScope.userLoginError = null;
-            $rootScope.user = user;
-          } else {
-            $rootScope.userLoginError = null;
-            $rootScope.user = null;
-          }
+          if (error)
+            $rootScope.user.auth.lastError = error;
+          else if (user)
+            $rootScope.user.auth.firebase = user;
+          else
+            $rootScope.user.auth.firebase = null;
         });
       })
     };
   }])
 
-  .controller('UserCtrl',
-              ['$scope', '$rootScope', 'auth',
-               function($scope, $rootScope, auth) {
+  .controller('AppCtrl',
+              ['$scope', '$rootScope', '$window', 'userSvc', 'appurl', 'client_id',
+               function($scope, $rootScope, $window, userSvc, appurl, client_id) {
+
     $scope.user = function() {
       return $rootScope.user;
     };
-    $scope.signin = function() {
-      auth.client.login('persona');
+    $scope.signedIn = function() {
+      return $rootScope.user.auth.firebase? true : false;
     };
-    $scope.signout = function() {
-      auth.client.logout();
-      $rootScope.user = null;
-    };
+
+    $scope.signin = function() { userSvc.login(); };
+    $scope.signout = function() { userSvc.logout(); };
+    $scope.connect = function(service) { userSvc.connect(service); };
+  }])
+
+  .run(['$location', '$http', 'userSvc',
+        function($location, $http, userSvc) {
+    var url = purl($location.absUrl());
+    var access_token = url.fparam('access_token') || url.fparam('/access_token');
+    var account = url.fparam('account') || url.fparam('/account');
+    if (access_token) {
+      userSvc.singlyConnected(access_token, account);
+    }
   }]);
