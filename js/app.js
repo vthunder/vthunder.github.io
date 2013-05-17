f30984a54014aabd25fef65a8f79c39937c6840b
Dan Mills <code@sandmill.org>
2013-05-17 10:42:43 -0700

    refactor services, rely more on events

--- js/app.js
+++ js/app.js
@@ -1,72 +1,52 @@
-$(document).foundation();
-
 // find out host we're on
 // sadtimes: global
 var appurl = document.createElement('a');
 appurl.href = "/";
-appurl = appurl.href
+appurl = appurl.href;
 
 angular.module("app", ['firebase'])
 
+  .value('purl', purl)
+  .value('appurl', appurl)
+  .value('firebaseurl', "https://openhome.firebaseio.com/")
   .value('client_id', (appurl == "http://localhost:8000"?
                        "798c72ca5049c8da83542ec260ecf9e9" : // dev sandbox
                        "09d889f820287c976065fb270bdf987d")) // vthunder.github.io
-  .value('firebaseurl', "https://openhome.firebaseio.com/")
 
-  .factory('dbRoot', ['firebaseurl', function(firebaseurl) {
-    return new Firebase(firebaseurl);
-  }])
+  .factory('authService', function($rootScope, firebaseurl) {
 
-  .factory('authSvc',
-           ['$rootScope', 'dbRoot', 'angularFire', 'firebaseurl',
-            function($rootScope, dbRoot, angularFire, firebaseurl) {
-
-    var auth = {};
+    var user = {};
 
     return {
-      authInfo: function() {
-        return auth;
-      },
-      signedIn: function() {
-        return auth.id? true : false;
-      },
       login: function() {
         this.client.login('persona');
       },
       logout: function() {
         this.client.logout();
-        auth.id = null;
+        delete user.id;
+        $rootScope.$broadcast('authStateChange', user);
       },
-      client: new FirebaseAuthClient(dbRoot, function(error, user) {
-        if (error)
-          auth.lastError = error;
-        else if (user)
-          auth.id = user.id;
-        else
-          auth.id = null;
-        $rootScope.$broadcast('authStateChange');
-      })
+      client: new FirebaseAuthClient(
+        new Firebase(firebaseurl), function(error, userInfo) {
+          if (error)
+            user.lastAuthError = error;
+          else if (userInfo)
+            user.id = userInfo.id;
+          else
+            delete user.id;
+          $rootScope.$broadcast('authStateChange', user);
+        })
     };
-  }])
+  })
 
-  .factory('connector', 
-           ['$rootScope', '$location', '$http', '$window',
-            'authSvc', 'client_id', 'firebaseurl',
-            function($rootScope, $location, $http, $window,
-                     authSvc, client_id, firebaseurl) {
-    var accts = {};
-    var data = {};
+  .factory('singly', function($rootScope, $location, $http, $window,
+                              purl, appurl, client_id) {
+    var singly = {};
 
     return {
-      userAccts: function() {
-        return accts;
-      },
-      userData: function() {
-        return data;
-      },
       connect: function(service) {
         var redirect_uri = encodeURIComponent(appurl);
-        var access_token = accts.singly? accts.singly.access_token : "";
+        var access_token = singly.access_token? singly.access_token : "";
 
         var url = "https://api.singly.com/oauth/authenticate" +
           "?client_id=" + client_id +
@@ -86,47 +66,52 @@ angular.module("app", ['firebase'])
         }
       },
       connectComplete: function(access_token, account) {
-        accts.singly = {
-          access_token: access_token,
-          account: account
-        };
-        $rootScope.$broadcast('connectComplete');
-        $http.get("https://api.singly.com/profile?access_token=" + access_token)
-          .success(function(profile) {
-            data.profile = profile;
-            $rootScope.$broadcast('profileUpdated');
-          })
-          .error(function(err) {
-            alert("error: " + err);
-          });
+        singly.access_token = access_token;
+        singly.account = account;
+        $rootScope.$broadcast('singlyConnected', singly);
       },
+      getProfile: function() {
+        return $http.get("https://api.singly.com/profile?access_token=" +
+                         singly.access_token);
+      }
     };
-  }])
+  })
 
-  .controller('AppCtrl',
-              ['$scope', 'authSvc', 'connector',
-               function($scope, authSvc, connector) {
+  .controller('AppCtrl', function($scope, authService, singly) {
 
-    var apply = function() { $scope.$safeApply($scope); };
-    $scope.$on('authStateChange', apply);
-    $scope.$on('profileUpdated', apply);
+    $scope.user = {
+      profile: {
+        name: "Anonymous"
+      }
+    };
 
-    $scope.user = function() { return authSvc.userInfo(); };
-    $scope.userAccts = function() { return connector.userAccts(); };
-    $scope.userData = function() { return connector.userData(); };
-    $scope.signedIn = function() { return authSvc.signedIn(); };
-    $scope.signin = function() { authSvc.login(); };
-    $scope.signout = function() { authSvc.logout(); };
-    $scope.connect = function(service) { connector.connect(service); };
-  }])
+    $scope.$on('authStateChange', function(event, user) {
+      $scope.user = user;
+      $scope.user.profile = {name: "Anonymous"};
+      $scope.$safeApply($scope);
+
+      // checks for OAuth token in url params - XXX should this go elsewhere?
+      singly.checkUrlParams();
+    });
+    $scope.$on('singlyConnected', function() {
+      singly.getProfile()
+        .success(function(profile) {
+          $scope.user.profile = profile;
+          $scope.$safeApply($scope);
+        });
+    });
+
+    $scope.signedIn = function() { return $scope.user.id? true : false; };
+    $scope.signin = function() { authService.login(); };
+    $scope.signout = function() { authService.logout(); };
+    $scope.connect = function(service) { singly.connect(service); };
+  })
 
   // init
 
-  .run(['$rootScope', 'connector',
-        function($rootScope, connector) {
-    //when you add it to the $rootScope variable, then it's accessible
-    //to all other $scope variables.
-    $rootScope.$safeApply = function($scope, fn) {
+  .run(function($rootScope) {
+    // provides $scope.$safeApply (for any sub-scope)
+    $rootScope.$safeApply = function ($scope, fn) {
       fn = fn || function() {};
       if($scope.$$phase) {
         //don't worry, the value gets set and AngularJS picks up on it...
@@ -138,7 +123,7 @@ angular.module("app", ['firebase'])
         $scope.$apply(fn); 
       }
     };
+  });
 
-    // Check for OAuth return (token in URL params)
-    connector.checkUrlParams();
-  }]);
+// Init Foundation JS components
+$(document).foundation();
